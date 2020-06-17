de2f73093247ea4c53bf4c32b075d2f5f5b1476c
wow-woo <fwpass@outlook.com>
2020-06-17 23:22:32 +0900

    sign up page added

--- index.js
+++ index.js
@@ -46,3 +46,107 @@ function colorChanger(count) {
     return;
   }
 }
+
+//nav sign in page
+const main = document.querySelector("main");
+const mainHome = [];
+
+for (let i = 0; i < main.children.length; i++) {
+  mainHome.push(main.children[i].cloneNode(true));
+}
+
+const singLi = document.querySelector(".sign-li");
+const signForm = document.querySelector(".sign-form");
+
+singLi.addEventListener("click", () => {
+  while (main.children.length) {
+    let i = 0;
+    const target = main.children[i];
+    const re = target.remove();
+    console.log("re", main.children.length);
+    i++;
+  }
+
+  signForm.classList.remove("hide");
+  main.appendChild(signForm);
+});
+
+//validation
+const action = document.querySelector(".action");
+const icons_i = document.querySelectorAll(".action > i");
+const times_i = document.querySelector("#times");
+const check_i = document.querySelector("#check");
+const arrow_i = document.querySelector("#arrow");
+const pw = document.querySelector("#password");
+let open = false;
+
+///authenticate User with data from database
+let result = "db";
+
+action.addEventListener("click", function validate() {
+  icons_i.forEach((icon) => {
+    icon.classList.add("hide");
+  });
+
+  if (!open) {
+    root.style.setProperty("--checking", "260px");
+    root.style.setProperty("--opacity", "1");
+
+    if (pw.value === result) {
+      check_i.classList.remove("hide");
+      root.style.setProperty("--validator", "skyblue");
+      action.dataset.content = "Good Password  ";
+    } else {
+      times_i.classList.remove("hide");
+      root.style.setProperty("--validator", "tomato");
+      action.dataset.content = "Wrong Password ";
+
+      setTimeout(() => {
+        validate();
+      }, 600);
+    }
+  } else {
+    pw.value = "";
+    arrow_i.classList.remove("hide");
+
+    root.style.setProperty("--checking", 0);
+    root.style.setProperty("--opacity", 0);
+    root.style.setProperty("--validator", "dodgerblue");
+  }
+  open = !open;
+});
+
+///set password visible or hidden
+const scout = document.querySelector(".eye");
+const openEye = document.querySelector("#openEye");
+const closeEye = document.querySelector("#closeEye");
+const eye_i = document.querySelectorAll(".eye > i");
+
+scout.addEventListener("click", () => {
+  const type = pw.getAttribute("type");
+
+  eye_i.forEach((eye) => {
+    eye.classList.add("hide");
+  });
+
+  if (type === "text") {
+    pw.setAttribute("type", "password");
+    closeEye.classList.remove("hide");
+  } else {
+    pw.setAttribute("type", "text");
+    openEye.classList.remove("hide");
+  }
+});
+
+//home
+const home = document.querySelector(".nav-link");
+home.addEventListener("click", () => {
+  console.log("first", main.firstElementChild);
+  main.firstElementChild.remove();
+
+  mainHome.forEach((node) => {
+    main.appendChild(node);
+    console.log(node.value);
+    console.log(main.children);
+  });
+});
